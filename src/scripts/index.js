0b032cb360b8b81fd5b1acdae879285bfe41afc9
Frederick Chon <frederick.chon@gmail.com>
2020-02-11 17:02:23 -0800

    added API call eventlisteners to render data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,13 +13,16 @@ h1.textContent = `Perfect Brew`;
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
+app.appendChild(logo);
+app.appendChild(h1);
+
 const search = document.createElement('div');
 search.setAttribute('class', 'search')
 
 const searchField = document.createElement('input');
 searchField.setAttribute('class', 'searchField');
 searchField.setAttribute('type', 'text');
-searchField.setAttribute('placeholder', 'Search for your favorite food pairings!');
+searchField.setAttribute('placeholder', 'Find your perfect food and beer pairing!');
 
 const searchBtn = document.createElement('button');
 searchBtn.setAttribute('class', 'searchBtn');
@@ -29,25 +32,65 @@ const searchBtnImg = document.createElement('img');
 searchBtnImg.setAttribute('class', 'searchBtnImg')
 searchBtnImg.src = '/src/images/search.svg';
 
-
-
-app.appendChild(logo);
-app.appendChild(h1);
-app.appendChild(container);
 app.appendChild(search);
 search.appendChild(searchField);
 search.appendChild(searchBtn);
 searchBtn.appendChild(searchBtnImg);
 
+searchBtn.addEventListener('click', () => getResults(searchField.value.toString()));
+searchField.addEventListener('keypress', function(e) {
+    if (e.keyCode == 13) {
+        let searchItem = searchField.value.toString();
+        getResults(searchItem);
+        console.log(`searchInput is ${searchItem}`);
+    }
+});
+
+app.appendChild(container);
 
 
-fetch('https://api.punkapi.com/v2/beers?food=sushi')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
+async function getResults(input) {
+    try {
+        let result = await fetch(`https://api.punkapi.com/v2/beers?food=${input}`);
+        let data = await result.json();
+        // console.log(data);
+        // console.log(`status: ${data.status}`)
+        container.textContent = ''; // Clear container before next query
+        data.forEach(beer => {
+            // Create div with card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = beer.name;
+
+            const img = document.createElement('img');
+            img.src = `${beer.image_url}`;
+
+            const p = document.createElement('p');
+            p.textContent = beer.description;
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(img);
+            card.appendChild(p);
+
+            
+        })
+    } catch(err) {
         console.log(err);
-    })
+    }
+}
+
+
+
+// fetch('https://api.punkapi.com/v2/beers?food=sushi')
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
